334ae79faf79eb8724f62cb159788d4afd9ca0fe
HandaMegha <mmeghahanda@gmail.com>
2023-01-10 19:48:46 +0530

    added background in home page

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./home.css";
 import { Link } from "react-router-dom"; 
+import { Icon } from "semantic-ui-react";
 
 const Home = () => {
     return(
         <div className="bg">
-            <h2> Home Page </h2>
-            <Link to="/usersList"> Users List </Link>
+            <Link to="/usersList" className="users-link"> <Icon name="user" /> Users </Link>
         </div>
     )
 }
